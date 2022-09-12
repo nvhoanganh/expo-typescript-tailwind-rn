61775ec7611d8a5b83aa037446fa0881fe675515
Alicia Nguyen <anguyen@bunnings.com.au>
2022-09-12 21:42:08 +1000

    remove lib

--- src/demoScreens/SketchPad/SketchPad.tsx
+++ src/demoScreens/SketchPad/SketchPad.tsx
@@ -1,17 +1,10 @@
-import { SketchCanvas } from "@terrylinla/react-native-sketch-canvas";
 import React from "react";
 import { StyleSheet, View } from "react-native";
 
 const SketchPad: React.FC = () => {
   return (
     <View style={styles.container}>
-      <View style={{ flex: 1, flexDirection: "row" }}>
-        <SketchCanvas
-          style={{ flex: 1 }}
-          strokeColor={"red"}
-          strokeWidth={7}
-        />
-      </View>
+      <View style={{ flex: 1, flexDirection: "row" }}></View>
     </View>
   );
 };
